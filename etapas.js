fd09e8575ab3624e75ab5c899a8b504a0dae0efe
RomeuCajamba <117159526+romeucajamba@users.noreply.github.com>
2023-04-11 17:37:51 +0100

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Romeu Cajamba',
+                partido: 'MPRLP',
+                fotos: [
+                    {url:'Romeu.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'Romeu Tomás',
+                partido: 'RTC',
+                fotos: [
+                    {url: 'Tomas.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Presidente',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Cajamba',
+                Partido: 'RTPJ',
+                Vice: 'Tomás',
+                fotos: [
+                    {url:'Cajmba.jpg', legenda: 'Presidente'},
+                    {url:'romeu (2).jpg', legenda: 'Vice-presidente', small: true}
+                ]
+            },
+            {
+                numero:84,
+                name:'Tomás',
+                partido: 'UPLR',
+                vice: 'Tomyson',
+                fotos: [
+                    {url:'Tomy.jpg', legenda: 'Presidente'},
+                    {url: ' Roger.jpg', legenda: 'Vice-presidente', small: true}
+                ]
+            }
+
+        ]
+    }
+];
+ 
